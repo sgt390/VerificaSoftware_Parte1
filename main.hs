d60a9b0b72afab1f01b659a9262bcb07cb1993e7
sgt390 <stefano.sgt39@gmail.com>
2020-01-25 12:56:27 +0100

    main comments

--- main.hs
+++ main.hs
@@ -9,9 +9,11 @@ import FunctionRenaming.State
 
 
 ----------------------------- REC Program Examples -----------------------------
+-- KEEP ONLY ONE UNCOMMENTED --
 ---inp = "x=2 g (1) f (x) = x+20 g (y) = f(y)" -- simple
---inp = "x=2 y=4 x=2 z=70 (f (g(1))) f(x)=x+1 g(x)=g(x)" -- unending calls (def)
-inp = "x=2 y=4 x=2 z=70 (f (g(1))) f(x)=7 g(x)=g(x)" -- vall by name
+---inp = "x=2 (g (6)) (g(z) = 3+g(3+z)+3)" -- undending cycle
+
+inp = "x=2 y=4 x=2 z=70 (f (g(1))) f(x)=7 g(x)=g(x)" -- call by name (g is ignored)
 --g(3) = f(4,h(3)+1) = 4 + (h(3)+1) = 4 + (1+(1+(1+1000)) +1) = 1008
 
 {-
@@ -25,21 +27,31 @@ inp = " \
 -}
 
 ---inp = "x=1 g(500) g(x)=x+h() h()=x" -- variables scope 501
+--------------------------------------------------------------------------------
 
+-- Syntactic parting --
 readp :: Program
 readp = let [(a,b)] = parse parseREC inp in substFun a
 
+-- (testing) output the parsed program and the error (if exists) --
 debugp = \input -> let [(a,b)] = (parse parseREC input) in show a ++ "++++++++++++++++" ++ b
 
--- evaluates the term t given an enviroment e
+-- Semantics of the term 't' given an enviroment 'e' and a function enviroment 'd',
+-- given k=maximum number of applications of the function f to itself.
+-- eg. k=4 => f(f(f(Bottom))))
 evalk :: Program -> Int -> Partial Int
 evalk (d, t, e) = \k -> semantics t (f_n f k bottom) env 
                    where env = envt e
-                         f = functional d env -- functional induced by d (calculated only once)
+                         f = functional d env -- functional induced by d (computed only once)
 
+-- Compute the least upper bount of the functional induced by the definition of the
+-- function enviroment defined in the program p, which its fixed point (one of many).
+-- Haskell helps with this task, because as soon as a value different from Undef
+-- is found, "head" returns that value, without computing the rest (lazy evaluation).
 evaluate :: Program -> Partial Int
 evaluate p = head [val | val <- map eval [0..], val /= Undef]
                 where eval = evalk p
 
+-- Call the function "main" to compute the uncommented program.
 main = putStrLn (show (evaluate (readp)))
 
